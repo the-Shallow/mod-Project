943b92fc6b12045d8b7d2c226e2977d7b5f48b5c
the-Shallow <theshallow27@gmail.com>
2021-07-18 23:47:31 +0530

    Changed viewController.js

--- Controller/viewsController.js
+++ Controller/viewsController.js
@@ -39,7 +39,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
 });
 
 exports.getLoginForm = catchAsync((req, res, next) => {
-  res
+  return res
     .status(200)
     .render('login', {
       title: 'Login into your Account',
