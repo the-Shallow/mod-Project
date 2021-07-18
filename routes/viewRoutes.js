fd29aa0c08479e7b2895d8331f4c9eda50b154e8
the-Shallow <theshallow27@gmail.com>
2021-07-18 23:58:51 +0530

    Updated viewController.js

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,13 +7,18 @@ const router = express.Router();
 
 // router.use(authController.isLoggedIn);
 
-router.get('/', bookingController.createBookingCheckout,authController.isLoggedIn, viewsController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsController.getOverview
+);
 
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 
-router.get('/signup',authController.isLoggedIn,viewsController.getSignupForm)
+router.get('/signup', authController.isLoggedIn, viewsController.getSignupForm);
 
 router.get('/me', authController.protect, viewsController.getAccount);
 
