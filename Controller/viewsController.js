44e3ec082cc39d916131f5e42e41fe07ebd01be1
the-Shallow <theshallow27@gmail.com>
2021-07-19 13:48:40 +0530

    Updated Code

--- Controller/viewsController.js
+++ Controller/viewsController.js
@@ -41,10 +41,6 @@ exports.getTour = catchAsync(async (req, res, next) => {
 exports.getLoginForm = catchAsync( async (req, res, next) => {
   res
     .status(200)
-    .set(
-      'Content-Security-Policy',
-      "connect-src 'self' https://cdnjs.cloudflare.com"
-    )
     .render('login', {
       title: 'Login into your Account',
     });
