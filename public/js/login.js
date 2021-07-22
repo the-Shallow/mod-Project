4ba292125c8722f69744c8641c83e087f5f2ae06
the-Shallow <theshallow27@gmail.com>
2021-07-22 18:55:02 +0530

    Updated code

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,43 @@
 import axios from 'axios';
 import { showAlerts } from './alerts';
 
-export const login = async (email, password) => {
+// export const login = async (email, password) => {
+//   try {
+//     const res = await axios({
+//       method: 'POST',
+//       url: '/api/v1/users/login',
+//       data: {
+//         email,
+//         password,
+//       },
+//     });
+
+//     if (res.data.status === 'success') {
+//       showAlerts('success', 'Logged In succesfully');
+//       window.setTimeout(() => {
+//         location.assign('/');
+//       }, 1000);
+//     }
+//   } catch (err) {
+//     // console.log('Hello');
+//     console.log(err);
+//     showAlerts('error', err.response.data.message);
+//   }
+// };
+
+// export const logout = async () => {
+//   try {
+//     const res = await axios({
+//       method: 'GET',
+//       url: '/api/v1/users/logout',
+//     });
+//     if (res.data.status === 'success') location.reload(true);
+//   } catch (err) {
+//     showAlerts('error', 'Error Logging out');
+//   }
+// };
+
+export async function login(email, password) {
   try {
     const res = await axios({
       method: 'POST',
@@ -14,26 +50,12 @@ export const login = async (email, password) => {
     });
 
     if (res.data.status === 'success') {
-      showAlerts('success', 'Logged In succesfully');
+      showAlerts('success', 'Logged in successfully');
       window.setTimeout(() => {
         location.assign('/');
-      }, 1000);
+      }, 500);
     }
   } catch (err) {
-    // console.log('Hello');
-    console.log(err);
-    showAlerts('error', err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
-};
-
-export const logout = async () => {
-  try {
-    const res = await axios({
-      method: 'GET',
-      url: '/api/v1/users/logout',
-    });
-    if (res.data.status === 'success') location.reload(true);
-  } catch (err) {
-    showAlerts('error', 'Error Logging out');
-  }
-};
+}
